b0ebdb0bdb1bbc68c17f1c19610293baeaafdc4e
lehua01 <lehua01@example.com>
2025-04-23 17:14:32 -0700

    Auto-commit: 2025-04-23 17:14:32

--- add_line_to_cron.sh
+++ add_line_to_cron.sh
@@ -2,3 +2,4 @@
 NEW_CRON_JOB="@reboot cd /Users/zlh/zoulehua01.github.io && nohup ./auto-push.sh > auto-push.log 2>&1 &"
 TEMP_CRON_FILE=$(mktemp)
 crontab -l > "" 2>/dev/null
+if ! grep -Fq "" ""; then
