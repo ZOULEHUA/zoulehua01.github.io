8f3e53df548e3e48016efa513587649785e597ce
lehua01 <lehua01@example.com>
2025-04-23 17:04:32 -0700

    Auto-commit: 2025-04-23 17:04:32

--- auto-push.sh
+++ auto-push.sh
@@ -1,7 +1,1 @@
 #!/usr/bin/env bash
-
-fswatch -o . | while read num; do
-    [ -f .git/index.lock ] && rm .git/index.lock
-    echo "Changes detected, committing..."
-    # Add additional Git commands here, like add, commit, and push
-done
